bb3fe1527b73fa5be78a0bbec3742c24538015d0
richarthurs <arthursrich@gmail.com>
2016-01-15 20:24:18 -0800

    First Commit!
    The initial commit

---
+++ project/main.c
@@ -0,0 +1,39 @@
+#include "driverlib/pin_map.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+int main(void) {
+SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ); // setup clock
+
+// Config computer-side UART
+SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);// Enable UART hardware
+SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);// Enable Pin hardware
+GPIOPinConfigure(GPIO_PA0_U0RX);// Configure GPIO pin for UART RX line
+GPIOPinConfigure(GPIO_PA1_U0TX);// Configure GPIO Pin for UART TX line
+GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1); // set pins for UART
+UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 9600,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+// Config GPS-side UART
+SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);// Enable UART hardware
+SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);// Enable Pin hardware
+GPIOPinConfigure(GPIO_PC4_U1RX);// Configure GPIO pin for UART RX line
+GPIOPinConfigure(GPIO_PC5_U1TX);// Configure GPIO Pin for UART TX line
+GPIOPinTypeUART(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5); // set pins for UART
+UARTConfigSetExpClk(UART1_BASE, SysCtlClockGet(), 9600,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+
+
+while (1){
+
+UARTCharPut(UART0_BASE, UARTCharGet(UART1_BASE));
+
+
+
+}
+}
